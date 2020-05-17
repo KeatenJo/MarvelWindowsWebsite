2370c82ed6e8d39fd28f268848d965cfb68217e1
Keaten Jones <keatenjones16@gmail.com>
2020-05-16 19:16:34 -0600

    implemented Reviews Page and css slideshow

--- src/server/public/methods.js
+++ src/server/public/methods.js
@@ -18,3 +18,17 @@ function myFunction() {
 	}
 };
 
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+	  var i;
+	  var x = document.getElementsByClassName("reviewSlides");
+	  for (i = 0; i < x.length; i++) {
+		      x[i].style.display = "none";
+		    }
+	  slideIndex++;
+	  if (slideIndex > x.length) {slideIndex = 1}
+	  x[slideIndex-1].style.display = "block";
+	  setTimeout(carousel, 4000); // Change image every 2 seconds
+}
