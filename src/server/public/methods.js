080af97888a76bf8df24d42e1db4095270f5a220
Keaten Jones <keatenjones16@gmail.com>
2020-10-19 17:38:04 -0600

    added bootstrap4 mobile support for about us page, index.html, services.html.

--- src/server/public/methods.js
+++ src/server/public/methods.js
@@ -22,13 +22,14 @@ var slideIndex = 0;
 carousel();
 
 function carousel() {
-	  var i;
-	  var x = document.getElementsByClassName("reviewSlides");
-	  for (i = 0; i < x.length; i++) {
-		      x[i].style.display = "none";
-		    }
-	  slideIndex++;
-	  if (slideIndex > x.length) {slideIndex = 1}
-	  x[slideIndex-1].style.display = "block";
-	  setTimeout(carousel, 4000); // Change image every 2 seconds
+	var i;
+	var x = document.getElementsByClassName("reviewSlides");
+	for (i = 0; i < x.length; i++) {
+		x[i].style.opacity = 0;
+	}
+	slideIndex++;
+	if (slideIndex > x.length) {slideIndex = 1}
+	x[slideIndex-1].style.opacity = 1;
+
+	setTimeout(carousel, 4000); // Change image every 2 seconds
 }
